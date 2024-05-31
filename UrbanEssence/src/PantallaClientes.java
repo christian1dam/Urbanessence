f0deb216d30bc43b9689a763220a61bbf390fa26
INFORMATICA\dam_mmd <danmarmar7@alu.edu.gva.es>
2024-05-31 12:37:14 +0200

    proyecto IntellIJ renombrado de intellIJ -> UrbanEssence, como estaba antes

---
+++ UrbanEssence/src/PantallaClientes.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class PantallaClientes extends JFrame {
+    private JPanel JPanelClientes;
+    private JButton btnBienvenido;
+    private JButton btnCiudades;
+    private JButton btnEmpleados;
+    private JButton btnTiendas;
+    private JButton btnTipo;
+    private JButton btnProductos;
+    private JButton btnProveedores;
+    private JButton btnPedidos;
+    private JButton btnClientes;
+    private JButton btnAnyadir;
+    private JButton btnEditar;
+    private JButton btnBorrar;
+    private JButton btnBuscar;
+    private JTable table1;
+    private JLabel lblLogo;
+
+    public PantallaClientes(){
+        super("Pantalla Clientes");
+        setContentPane(JPanelClientes);
+        ImageIcon imageLogo = new ImageIcon("imagenes/LogoModificado.png");
+        ImageIcon iconoClientes = new ImageIcon("imagenes/iconoClientes.png");
+        lblLogo.setIcon(imageLogo);
+        btnClientes.setIcon(iconoClientes);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new PantallaClientes();
+        frame.setVisible(true);
+        frame.setSize(1200,800);
+        frame.setLocationRelativeTo(null);
+
+
+
+    }
+}
