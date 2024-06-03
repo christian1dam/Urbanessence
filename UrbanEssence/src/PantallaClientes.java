022265672d580415cbb300cc3591d16885792dc6
INFORMATICA\dam_mmd <danmarmar7@alu.edu.gva.es>
2024-06-03 09:07:06 +0200

    Commit avances

--- UrbanEssence/src/PantallaClientes.java
+++ UrbanEssence/src/PantallaClientes.java
@@ -1,39 +1,201 @@
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonUI;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class PantallaClientes extends JFrame {
-    private JPanel JPanelClientes;
-    private JButton btnBienvenido;
-    private JButton btnCiudades;
-    private JButton btnEmpleados;
-    private JButton btnTiendas;
+public class PantallaClientes extends JFrame{
+    private JPanel panelGeneral;
+    private JButton btnPedido;
+    private JButton btnCliente;
+    private JButton btnProveedor;
+    private JButton btnProducto;
     private JButton btnTipo;
-    private JButton btnProductos;
-    private JButton btnProveedores;
-    private JButton btnPedidos;
-    private JButton btnClientes;
-    private JButton btnAnyadir;
-    private JButton btnEditar;
-    private JButton btnBorrar;
-    private JButton btnBuscar;
-    private JTable table1;
+    private JButton btnTienda;
+    private JButton btnEmpleado;
+    private JButton btnCiudad;
+    private JButton btnPerfil;
+    private JButton btnLogout;
+    private JButton btnAdd;
+    private JButton btnEdit;
+    private JButton btnDelete;
+    private JTextField buscarTextField;
     private JLabel lblLogo;
+    private JTable tabla;
+    private JButton btnBuscar;
 
-    public PantallaClientes(){
-        super("Pantalla Clientes");
-        setContentPane(JPanelClientes);
-        ImageIcon imageLogo = new ImageIcon("imagenes/LogoModificado.png");
-        ImageIcon iconoClientes = new ImageIcon("imagenes/iconoClientes.png");
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+
+    public PantallaClientes() {
+        super("Panel Clientes");
+        setContentPane(panelGeneral);
+        ImageIcon imageLogo = new ImageIcon("imagenes/Logo.png");
         lblLogo.setIcon(imageLogo);
-        btnClientes.setIcon(iconoClientes);
+
+        crearTabla();
+        cargarIconos();
+        formatoBotones();
+        cargarClientes();
+
+        btnTienda.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        JFrame frame = new PantallaTiendas();
+                        frame.setVisible(true);
+                        frame.setSize(1080,670);
+                        frame.setLocationRelativeTo(null);
+                    }
+                });
+            }
+        });
+
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        JFrame frame = new PantallaEditarTiendas();
+                        frame.setVisible(true);
+                        frame.setSize(745,620);
+                        frame.setLocationRelativeTo(null);
+                    }
+                });
+            }
+        });
     }
 
+    private void cargarIconos() {
+        ImageIcon iconoClientes = new ImageIcon("imagenes/iconoClientesBlanco.png");
+        btnCliente.setIcon(iconoClientes);
+        btnCliente.setUI(new BasicButtonUI());
+        
+        ImageIcon iconoPedidos = new ImageIcon("imagenes/iconoPedidos.png");
+        btnPedido.setIcon(iconoPedidos);
+        btnPedido.setUI(new BasicButtonUI());
+
+        ImageIcon iconoProveedores = new ImageIcon("imagenes/iconoProveedores.png");
+        btnProveedor.setIcon(iconoProveedores);
+
+        ImageIcon iconoProductos = new ImageIcon("imagenes/iconoProductos.png");
+        btnProducto.setIcon(iconoProductos);
+
+        ImageIcon iconoTipo = new ImageIcon("imagenes/iconoTipo.png");
+        btnTipo.setIcon(iconoTipo);
+
+        ImageIcon iconoTiendas = new ImageIcon("imagenes/iconoTiendas.png");
+        btnTienda.setIcon(iconoTiendas);
+
+        ImageIcon iconoEmpleados = new ImageIcon("imagenes/iconoEmpleados.png");
+        btnEmpleado.setIcon(iconoEmpleados);
+
+        ImageIcon iconoCiudades = new ImageIcon("imagenes/iconoCiudades.png");
+        btnCiudad.setIcon(iconoCiudades);
+
+
+        ImageIcon iconoLogOut = new ImageIcon("imagenes/iconoLogOut.png");
+        btnLogout.setIcon(iconoLogOut);
+
+
+        ImageIcon iconoPerfil = new ImageIcon("imagenes/iconoPerfil.png");
+        btnPerfil.setIcon(iconoPerfil);
+        btnPerfil.setUI(new BasicButtonUI());
+        btnPerfil.setFocusPainted(false);
+        btnPerfil.setBorderPainted(false);
+
+        ImageIcon imgBuscar = new ImageIcon("imagenes/iconoLupa.png");
+        btnBuscar.setIcon(imgBuscar);
+        btnBuscar.setUI(new BasicButtonUI());
+        btnBuscar.setFocusPainted(false);
+        btnBuscar.setBorderPainted(false);
+
+        btnAdd.setUI(new BasicButtonUI());
+        btnAdd.setFocusPainted(false);
+        btnAdd.setBorderPainted(false);
+
+        btnDelete.setUI(new BasicButtonUI());
+        btnDelete.setFocusPainted(false);
+        btnDelete.setBorderPainted(false);
+
+        btnEdit.setUI(new BasicButtonUI());
+        btnEdit.setFocusPainted(false);
+        btnEdit.setBorderPainted(false);
+
+    }
+
+    private void formatoBotones() {
+
+        btnPedido.setBorder(null);
+        btnPedido.setContentAreaFilled(false);
+
+        btnProveedor.setBorder(null);
+        btnProveedor.setContentAreaFilled(false);
+
+        btnProducto.setBorder(null);
+        btnProducto.setContentAreaFilled(false);
+
+        btnTipo.setBorder(null);
+        btnTipo.setContentAreaFilled(false);
+
+        btnTienda.setBorder(null);
+        btnTienda.setContentAreaFilled(false);
+
+        btnEmpleado.setBorder(null);
+        btnEmpleado.setContentAreaFilled(false);
+
+        btnCiudad.setBorder(null);
+        btnCiudad.setContentAreaFilled(false);
+
+        btnLogout.setBackground(null);
+        btnLogout.setBorder(null);
+        btnLogout.setContentAreaFilled(false);
+    }
+
+    public void cargarClientes (){
+        if (DataManager.getClientes()){
+            clientes = DataManager.getListaClientes();
+            crearTabla();
+        }
+    }
+
+    private void crearTabla() {
+        String[][] data = new String[clientes.size()][6];
+
+        for (int i = 0; i < clientes.size(); i++) {
+            String genero;
+            if (clientes.get(i).getGenero() == 1) {genero = "H";}else {genero = "M";}
+            data[i][0] = String.valueOf(clientes.get(i).getId());
+            data[i][1] = String.valueOf(clientes.get(i).getNombre());
+            data[i][2] = genero;
+            data[i][3] = String.valueOf(clientes.get(i).getFechaNac());
+            data[i][4] = String.valueOf(clientes.get(i).getTelefono());
+            data[i][5] = String.valueOf(clientes.get(i).getDireccion());
+        }
+
+        tabla.setModel(new DefaultTableModel(
+                data,
+                new String[]{"ID", "NOMBRE", "GENERO", "F.NACIMIENTO", "H.TELEFONO", "DIRECCION"}
+        ));
+        tabla.getTableHeader().setReorderingAllowed(false);
+        tabla.setEnabled(true);
+        tabla.setDefaultEditor(Object.class, null);
+    }
+
+
+
     public static void main(String[] args) {
-        JFrame frame = new PantallaClientes();
-        frame.setVisible(true);
-        frame.setSize(1200,800);
-        frame.setLocationRelativeTo(null);
-
-
-
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new PantallaClientes();
+                frame.setVisible(true);
+                frame.setSize(1080,670);
+                frame.setLocationRelativeTo(null);
+            }
+        });
     }
 }
